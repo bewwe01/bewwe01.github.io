71deb5e97ef3c3b141f5b78e9a043c842dd94a68
zuennatron <bscraeyen@gmail.com>
2026-03-22 17:14:04 +0100

    Configure GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Replace with your repository name if it's not a user/org site (e.g., '/porj/')
 })
